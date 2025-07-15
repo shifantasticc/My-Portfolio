f422fec72a70b28a5b8037e6f12c6716b4db9269
shifantasticc <shifastudyit@gmail.com>
2025-07-15 22:18:28 +0530

    Making Postfolio Responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,24 +13,21 @@ const About = () => {
               Know Who <span>I'M</span>
             </p>
             <p>
-              {' '}
               Hi Everyone, I am <span>Shifa Shaikh</span> from{' '}
               <span>Mumbai, India.</span> I am currently 3rd year BSc student of
               Information Technology at SNDT, Mumbai. I'm{' '}
               <span>Web developer</span> and{' '}
               <span>DSA in Java enthusiast.</span>
             </p>
-
             <p>
               I am looking forward to gain competency and exposure in{' '}
               <span>
+                {' '}
                 Web development, Data Structures and Algorithms and Machine
                 Learning.
               </span>
             </p>
-
             <p>
-              {' '}
               Apart from coding, some other activities that I love to do!
               <ul>
                 <li>Reading</li>
@@ -38,19 +35,16 @@ const About = () => {
                 <li>Learning Language & Culture</li>
               </ul>
             </p>
-
-            <div className="quote">
-              <p>
-                "We are what we repeatedly do. Excellence, then, is not an act,{' '}
-                <br />
-                but a habit."
-              </p>
-              <p>— Aristotle</p>
-            </div>
           </div>
           <div className="About-Img">
             <img src={Image} alt="Web Developer" className="About-image" />
           </div>
+        </div>
+        <div className="quote">
+          <p>
+            "We are what we repeatedly do. Excellence, then, is not an act, but
+            a habit." — Aristotle
+          </p>
         </div>
       </section>
       <Skills />
